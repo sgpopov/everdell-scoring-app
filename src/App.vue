9ac187f7f231c5e41300b1ae1180e416add28e6d
Svilen Popov <svil4ok@gmail.com>
2025-02-23 11:08:44 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
+  >
+    <h1 class="text-4xl font-bold mb-4">Everdell Scoring App</h1>
+  </div>
+</template>
