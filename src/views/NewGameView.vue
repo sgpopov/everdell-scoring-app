d238d9699285a335b7c964805683251199c92a46
Svilen Popov <svil4ok@gmail.com>
2025-02-24 12:53:12 +0200

    feat: add new view for creating a new game

--- src/views/NewGameView.vue
+++ src/views/NewGameView.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>new game view</div>
+  <NewGame :defaultPlayers="startPlayers" @startGame="onGameStarted" />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Player } from '@/types';
+import NewGame from '@/components/NewGame.vue';
+
+const startPlayers = ref(4);
+
+const onGameStarted = (players: Player[]) => {
+  console.log('players', players);
+};
+</script>
