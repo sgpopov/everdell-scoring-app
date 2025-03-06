ab8ce7782efae624697672464b9120ef8693456f
Svilen Popov <svil4ok@gmail.com>
2025-03-06 19:52:23 +0200

    fix: added base public path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '',
+  base: '/everdell-scoring-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
