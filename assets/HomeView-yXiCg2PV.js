c17d3063d7c3efc92bdea02acc7362d8bdaef5f4
Svilen Popov <svil4ok@gmail.com>
2025-03-06 19:57:41 +0200

    Updates

---
+++ assets/HomeView-yXiCg2PV.js
@@ -0,0 +1,1 @@
+import{c as r,o as s}from"./index-YkEAHLR4.js";const _=(e,o)=>{const t=e.__vccOpts||e;for(const[c,n]of o)t[c]=n;return t},a={};function f(e,o){return s(),r("div",null,"Home view")}const i=_(a,[["render",f]]);export{i as default};
